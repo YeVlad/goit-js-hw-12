fa91efaef9db486160cbe740b175034abffb1276
YeVlad <150618209+YeVlad@users.noreply.github.com>
2024-04-10 20:00:58 +0200

    working version

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,13 @@
 import { makeGallery } from './render-functions';
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
 
-export async function doFetch(aim, textToWait, gallery) {
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+export async function doFetch(aim, textToWait, gallery, refreshGallery) {
   const API_KEY = '16991331-df0a6792d36af314f174a3b15';
   const url = 'https://pixabay.com/api/';
 
@@ -13,7 +20,8 @@ export async function doFetch(aim, textToWait, gallery) {
     per_page: 9,
   });
 
-  textToWait.hidden = false;
+  textToWait.style.borderColor = 'black';
+  textToWait.style.borderBottomColor = 'transparent';
 
   fetch(`${url}?${params}`)
     .then(response => {
@@ -23,14 +31,30 @@ export async function doFetch(aim, textToWait, gallery) {
       return response.json();
     })
     .then(data => {
+      if (data.total == 0) {
+        iziToast.show({
+          title: 'Ops.',
+          titleColor: 'white',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          messageColor: 'white',
+          color: 'red',
+          position: 'topCenter',
+          timeout: '5000',
+        });
+        throw new Error(data.status);
+      }
       return makeGallery(data);
     })
     .then(newGallery => {
-      textToWait.hidden = true;
+      textToWait.style.borderColor = 'white';
+      textToWait.style.borderBottomColor = 'transparent';
       gallery.insertAdjacentHTML('beforeend', newGallery);
+      refreshGallery.refresh();
     })
     .catch(error => {
-      textToWait.hidden = true;
+      textToWait.style.borderColor = 'white';
+      textToWait.style.borderBottomColor = 'transparent';
       console.log(error);
     });
 }
--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
 import { doFetch } from './js/pixabay-api';
 
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 const form = document.querySelector('.form-search');
-const textWait = document.querySelector('.text-wait');
+const loader = document.querySelector('.loader');
+loader.style.borderColor = 'white';
+loader.style.borderBottomColor = 'transparent';
 const photoGallery = document.querySelector('.images-place');
+
+const book = new SimpleLightbox('.card .place-for-image a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 form.addEventListener('submit', handleSearch);
 
 async function handleSearch(event) {
   event.preventDefault();
+  photoGallery.innerHTML = '';
   const searchWord = event.currentTarget.elements.inputSearch.value;
-  await doFetch(searchWord, textWait, photoGallery);
+  await doFetch(searchWord, loader, photoGallery, book);
 }
